16158162a0aaa528978b283e2c78c093202302ab
Rodrigohernandezsan <rodrigoherandezsan@gamil.com>
2020-11-02 21:24:12 +0100

    db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,48 +1,48 @@
-'use strict';
+// 'use strict';
 
-const { MongoClient, ObjectID } = require('mongodb');
+// const { MongoClient, ObjectID } = require('mongodb');
 
-class Context {
-  constructor(url, enableLogging) {
-    this.client = new MongoClient(url, { useNewUrlParser: true });
-    this.enableLogging = enableLogging;
-    this.db = {};
-  }
-  log(message) {
-    if (this.enableLogging) {
-      console.info(message);
-    }
-  }
+// class Context {
+//   constructor(url, enableLogging) {
+//     this.client = new MongoClient(url, { useNewUrlParser: true });
+//     this.enableLogging = enableLogging;
+//     this.db = {};
+//   }
+//   log(message) {
+//     if (this.enableLogging) {
+//       console.info(message);
+//     }
+//   }
 
-  async connect(dbName) {
-    this.log('Connecting to the database...');
-    const server = await this.client.connect();
-    this.db = server.db(dbName);
-  }
+//   async connect(dbName) {
+//     this.log('Connecting to the database...');
+//     const server = await this.client.connect();
+//     this.db = server.db(dbName);
+//   }
 
-  prepareObjectIDs(documents) {
-    documents.forEach((document) => {
-      Object.keys(document).forEach((key) => {
-        const value = document[key];
+//   prepareObjectIDs(documents) {
+//     documents.forEach((document) => {
+//       Object.keys(document).forEach((key) => {
+//         const value = document[key];
 
-        if (value.$oid) {
-          document[key] = ObjectID.createFromHexString(value.$oid);
-        }
-      });
-    });
-  }
+//         if (value.$oid) {
+//           document[key] = ObjectID.createFromHexString(value.$oid);
+//         }
+//       });
+//     });
+//   }
 
-  async insertDocuments(collectionName, documents) {
-    this.prepareObjectIDs(documents);
+//   async insertDocuments(collectionName, documents) {
+//     this.prepareObjectIDs(documents);
 
-    const collection = this.db.collection(collectionName);
+//     const collection = this.db.collection(collectionName);
 
-    this.log(`Removing all documents from the '${collectionName}' collection...`);
-    await collection.deleteMany();
+//     this.log(`Removing all documents from the '${collectionName}' collection...`);
+//     await collection.deleteMany();
 
-    this.log(`Inserting documents into the '${collectionName}' collection...`);
-    await collection.insertMany(documents);
-  }
-}
+//     this.log(`Inserting documents into the '${collectionName}' collection...`);
+//     await collection.insertMany(documents);
+//   }
+// }
 
-module.exports = Context;
+// module.exports = Context;
